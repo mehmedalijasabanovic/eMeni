8f4f0fb66bdda372672bf6d2b8bf974676f67a64
mehmedalijasabanovic <mehmedalija.sabanovic@edu.fit.ba>
2025-11-14 19:17:05 +0100

    added AiChatLogs entitiy and configurator

---
+++ eMeni.Backend/eMeni.Domain/Entities/AiLogs/AiChatLogEntity.cs
@@ -0,0 +1,25 @@
+﻿
+#nullable disable
+using eMeni.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace eMeni.Infrastructure.Models;
+
+public sealed class AiChatLogEntity
+{
+    public int Id { get; set; }
+    public int? UserId { get; set; }
+
+    public Guid? SessionId { get; set; }
+
+    public string Message { get; set; }
+
+    public string Response { get; set; }
+
+    public DateTime? Timestamp { get; set; }
+
+    public string MessageType { get; set; }
+
+    public eMeniUserEntity User { get; set; }
+}
---
+++ eMeni.Backend/eMeni.Infrastructure/Database/Configurations/AiLogs/AiChatLogEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿
+using eMeni.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace eMeni.Infrastructure.Models.Configurations
+{
+    public partial class AiChatLogEntityConfiguration : IEntityTypeConfiguration<AiChatLogEntity>
+    {
+        public void Configure(EntityTypeBuilder<AiChatLogEntity> entity)
+        {
+            entity.Property(e => e.Message).IsRequired();
+            entity.Property(e => e.MessageType)
+                .HasMaxLength(20)
+                .HasDefaultValue("User");
+            entity.Property(e => e.SessionId).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.Timestamp)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+
+            entity.HasOne(d => d.User).WithMany(p => p.AiChatLogs)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__AiChatLog__UserI__6B24EA82");
+
+            OnConfigurePartial(entity);
+        }
+
+        partial void OnConfigurePartial(EntityTypeBuilder<AiChatLogEntity> entity);
+    }
+}
