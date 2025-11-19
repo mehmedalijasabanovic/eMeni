6834df35aa70cde2cf5cdc20532494c5b5e27d73
mehmedalijasabanovic <mehmedalija.sabanovic@edu.fit.ba>
2025-11-19 20:09:14 +0100

    implemented post API endpoint for city

---
+++ eMeni.Backend/eMeni.API/Controllers/CityController.cs
@@ -0,0 +1,17 @@
+﻿using eMeni.Application.Modules.Location.Commands.Create;
+
+namespace eMeni.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CityController(ISender sender):ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateCity(CreateCityCommand command,CancellationToken ct)
+        {
+            int id = await sender.Send(command, ct);
+
+            return Created(string.Empty, new { id });
+        }
+    }
+}
