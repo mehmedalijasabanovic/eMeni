599b2ff8fa1f2a29dd2a0e86cfacb88b1565aba0
mehmedalijasabanovic <mehmedalija.sabanovic@edu.fit.ba>
2025-12-24 21:38:21 +0100

    same as last commit

---
+++ eMeni.Backend/eMeni.Application/Modules/Menu/MenuCategory/Commands/Create/CreateMenuCategoryCommandHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace eMeni.Application.Modules.Menu.MenuCategory.Commands.Create
+{
+    public sealed class CreateMenuCategoryCommandHandler
+    {
+
+    }
+}
---
+++ eMeni.Backend/eMeni.Application/Modules/Menu/MenuItem/Commands/Create/CreateMenuItemCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using eMeni.Infrastructure.Models;
+
+namespace eMeni.Application.Modules.Menu.MenuItem.Commands.Create
+{
+    public sealed class CreateMenuItemCommandHandler(IAppDbContext db,IAuthorizationHelper auth):
+        IRequestHandler<CreateMenuItemCommand, int>
+    {
+        public async Task<int> Handle(CreateMenuItemCommand cmd, CancellationToken ct)
+        {
+            var categoryExists = await db.MenuCategories
+                .AnyAsync(x => x.Id == cmd.CategoryId && !x.IsDeleted, ct);
+
+            if (!categoryExists)
+                throw new eMeniNotFoundException("Category not found.");
+
+            var item = new MenuItemEntity
+            {
+                CategoryId = cmd.CategoryId,
+                ItemName = cmd.ItemName.Trim(),
+                ItemDescription = cmd.ItemDescription?.Trim(),
+                Price = cmd.Price.Trim(),
+                ImageUrl = cmd.ImageUrl
+            };
+
+            db.MenuItems.Add(item);
+            await db.SaveChangesAsync(ct);
+
+            return item.Id;
+        }
+    }
+}
